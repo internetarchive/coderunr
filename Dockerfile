856aa28b838fc64e7cee0c2d694b37787bf7135d
Tracey Jaquith <tracey@archive.org>
2023-01-25 18:33:15 -0800

    WiP

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM alpine
+# FROM python:3-alpine # xxx for pyinotify.py
 
 RUN apk add zsh caddy
+# RUN wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.30.8/yq_linux_amd64 && chmod +x /usr/local/bin/yq # xxx if yq from inside prevu..
 
 WORKDIR /app
 COPY . .
 
 CMD [ "/app/watcher.sh" ]
-
--- watcher.sh
+++ watcher.sh
@@ -24,3 +24,13 @@ $(hostname -f) {
 
 
 sleep 864000 # hang out for 10d for now until triggers and container mgmt setup
+
+# xxx could do instead?  ./pyinotify.py -e IN_CLOSE_WRITE -r /prevu
+
+# every 5s enumerate:
+for YML in $(ls /prevu/*/*/*/prevu.yml /prevu/internetarchive/prevu/main/*.yml 2>/dev/null); do
+
+  ./pyinotify.py -e IN_CLOSE_WRITE www/sf/includes/less/**/
+
+
+done
